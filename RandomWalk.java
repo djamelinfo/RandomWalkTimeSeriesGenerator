281cc63a7d1110baf79b253e50aea3644674adda
Djamel Edine <yagoubi.djamel@gmail.com>
2016-04-04 13:54:13 +0200

    Optimization

--- RandomWalk.java
+++ RandomWalk.java
@@ -27,20 +27,18 @@ import org.apache.commons.math3.random.JDKRandomGenerator;
 public class RandomWalkTsG {
 
     public static String[] randomWalk(int length) {
-        NormalDistribution n = new NormalDistribution(new JDKRandomGenerator(),
-                                                      0, 1); //mean 0 std 1 variance 1
+        NormalDistribution n = new NormalDistribution(new JDKRandomGenerator(),0, 1); //mean 0 std 1 variance 1
         String[] ts = new String[length];
+        double[] tstemp = new double[length];
         double[] e = new double[length - 1];
 
         for(int i = 0; i < e.length; i++) {
-            e[i] = (double) n.sample();
+            e[i] = n.sample();
         }
         ts[0] = "0.0";
         for(int i = 1; i < length; i++) {
-            ts[i] = (Double.
-                    parseDouble(ts[i - 1]) + e[i - 1]) + "";
+            ts[i] = (tstemp[i] = tstemp[i - 1] + e[i - 1])+"";
         }
-
         return ts;
     }
 
@@ -51,23 +49,17 @@ public class RandomWalkTsG {
     public static void main(String[] args) throws IOException, Exception {
 
         if(args.length < 2) {
-            System.err.println(
-                    "Usage: randomWalkTimeSeriesGenerator <FileName> <TimeSeriesNbr> <TimeSeriesSize>");
+            System.err.println("Usage: randomWalkTimeSeriesGenerator <FileName> <TimeSeriesNbr> <TimeSeriesSize>");
             System.exit(1);
         }
 
-        CSVWriter writer = new CSVWriter(new FileWriter(args[0] + "_" + args[1]
-                + "_" + args[2] + ".csv"), CSVWriter.DEFAULT_SEPARATOR,
-                                         CSVWriter.NO_QUOTE_CHARACTER);
+        CSVWriter writer = new CSVWriter(new FileWriter(args[0] + "_" + args[1]+ "_" + args[2] + ".csv"), CSVWriter.DEFAULT_SEPARATOR,CSVWriter.NO_QUOTE_CHARACTER);
 
         for(int i = 1; i < Integer.parseInt(args[1]) + 2; i++) {
             String[] ts = randomWalk(Integer.parseInt(args[2]) + 1);
-
             ts[0] = Integer.toString(i);
-
             writer.writeNext(ts);
             writer.flushQuietly();
-
         }
         writer.close();
     }
