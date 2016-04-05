a2c9660e0b41b7b6b58c51a12b5d28f7c14df0c5
Djamel Edine <yagoubi.djamel@gmail.com>
2016-04-05 20:31:19 +0200

    Update RandomWalk.java

--- src/main/java/fr/inria/randomWalk/RandomWalk.java
+++ src/main/java/fr/inria/randomWalk/RandomWalk.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+ package fr.inria.randomWalk;
 
 import com.opencsv.CSVWriter;
 import java.io.FileWriter;
